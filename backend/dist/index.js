31bd51bff4ee4eac8dd3a51d93a65731944eb874
dharmendra016 <dharmendra.078bct016@tcioe.edu.np>
2025-01-02 19:45:13 +0545

    dbconnection feature added

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,8 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dbConnect_1 = __importDefault(require("./utility/dbConnect"));
+require("dotenv/config");
 const app = (0, express_1.default)();
+const PORT = process.env.PORT || 8000;
+//connection
+(0, dbConnect_1.default)();
 app.get("/", (req, res) => {
     res.send("Hi there");
 });
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`Server started at port: ${PORT}`);
+});
